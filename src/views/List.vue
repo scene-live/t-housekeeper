d7224dca5e401f13c8575c16e71c71795a87886d
r.taga <r.taga@rtagaMBP.local>
2020-02-25 16:21:55 +0900

    一覧ページ用に router とファイルを追加した。

---
+++ src/views/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="list">
+    <h1>List</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'List',
+  components: {},
+};
+</script>
