f83df64d4628e7f3a93d25e5ec0e7f33eab6ce82
r.taga <r.taga@rtagaMBP.local>
2020-03-02 13:08:39 +0900

    ボタンのスタイルを調整した。

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -31,12 +31,22 @@ export default class Button extends Vue {}
     padding: 8px 5px;
     font-size: 1.6rem;
     border-radius: $borderRadius;
+    @media #{$pc} {
+      &:hover {
+        opacity: .8;
+      }
+    }
     @each $name, $color in $buttonColors {
       &-#{$name} {
         background: $color;
         @if($name == disabled) {
           color: #fff;
           opacity: .65;
+          @media #{$pc} {
+            &:hover {
+              opacity: .5;
+            }
+          }
         }
         @if ($name == tertiary or $name == disabled) {
           color: #fff;
